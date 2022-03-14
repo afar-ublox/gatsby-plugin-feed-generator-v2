0aeeaede40f30bcfafb084907e12cffa8db4960f
afar <ahmadfaryab@u-blox.com>
2022-03-14 16:54:31 +0500

    Add to Header only if addToHeader is true

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -2,34 +2,37 @@ import React from 'react'
 import urlJoin from 'url-join'
 import { withPrefix } from 'gatsby'
 exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
-  const { rss, json, feeds, baseUrl } = pluginOptions
-  let output = []
-  for (let feed of feeds) {
-    let siteUrl = feed.baseUrl || '/'
-    if (rss) {
-      let rssurl = urlJoin(siteUrl, feed.name).replace(/\/+$/, '') + '.xml'
-      output.push(
-        <link
-          rel="alternate"
-          key="gatsby-feed-rss"
-          type="application/rss+xml"
-          href={rssurl}
-        />
-      )
-    }
+  const { rss, json, feeds, baseUrl } = pluginOptions;
 
-    if (json) {
-      let jsonurl = urlJoin(siteUrl, feed.name).replace(/\/+$/, '') + '.json'
-      output.push(
-        <link
-          rel="alternate"
-          key="gatsby-feed-json"
-          type="application/json"
-          href={jsonurl}
-        />
-      )
+  let output = []
+
+  if(feed.addToHeader === true){
+    for (let feed of feeds) {
+      let siteUrl = feed.baseUrl || '/'
+      if (rss) {
+        let rssurl = urlJoin(siteUrl, feed.name).replace(/\/+$/, '') + '.xml'
+        output.push(
+          <link
+            rel="alternate"
+            key="gatsby-feed-rss"
+            type="application/rss+xml"
+            href={rssurl}
+          />
+        )
+      }
+
+      if (json) {
+        let jsonurl = urlJoin(siteUrl, feed.name).replace(/\/+$/, '') + '.json'
+        output.push(
+          <link
+            rel="alternate"
+            key="gatsby-feed-json"
+            type="application/json"
+            href={jsonurl}
+          />
+        )
+      }
     }
   }
-
   setHeadComponents(output)
 }
